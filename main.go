ccd957a5bc8e62b0781d627f13bbf03e0253c852
kapoorarpit <kapoorarpit2000@gmail.com>
2021-10-30 15:16:52 +0530

    _

--- main.go
+++ main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var collection *mongo.Collection
+
+func init() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	//ctx, _ := context.WithTimeout(context.Background(), 10+time.Second)
+	client, err := mongo.Connect(context.Background(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection = (*mongo.Collection)(client.Database("URLs").Collection("col"))
+
+	fmt.Println("Collection instance is ready")
+}
+
 func main() {
+	//client, _ = mongo.Connect(ctx, "mongodb://localhost:27017")
 	router := mux.NewRouter()
 	router.HandleFunc("/shorten", CreateEndpoint).Methods("POST")
 	router.HandleFunc("/expand/", ExpandEndpoint).Methods("POST")
@@ -18,7 +39,7 @@ func main() {
 }
 
 type URL struct {
-	LongURL  string    `json:"longURL,omitempty"`
+	LongURL  string    `json:"longURL,omitempty" bson:"_id,omitempty"`
 	shortURL string    `json:"shortURL,omitempty"`
 	date     time.Time `json:"date,omitempty"`
 }
@@ -28,6 +49,14 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func CreateEndpoint(w http.ResponseWriter, r *http.Request) {
+	// cache will be good option to check if data already exists
+	// we haev to check if shorten URL is existing or not
+	// the problem is if there is a user generating shortURL for long One
+	last_num := 0x0 // this will be the last generated url which will be retrieved from the database
+
+	i := 1 + last_num //increment i by one
+	print(i)
+	//upto 18 min
 
 }
 
@@ -38,3 +67,7 @@ func ExpandEndpoint(w http.ResponseWriter, r *http.Request) {
 func RedirectEndpoint(w http.ResponseWriter, r *http.Request) {
 
 }
+
+//database part skipped
+
+//skipped cluster at 14:40 to 16:00
